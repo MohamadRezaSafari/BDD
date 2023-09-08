fee8c03d6603f00eab9e8dd4d6f6a1e611f15500
Reza <Reza@DESKTOP-CKL538G>
2023-09-08 23:44:06 +0330

    Create Specflow biding methods

---
+++ BehaviorDrivenDevelopment.Tests.Behavior/BehaviorDrivenDevelopment.Services/PricingService.cs
@@ -0,0 +1,36 @@
+﻿namespace BehaviorDrivenDevelopment.Services;
+
+public interface IPricingService
+{
+    decimal GetBasketTotalAmount(Basket basket);
+}
+
+public class PricingService : IPricingService
+{
+    public decimal GetBasketTotalAmount(Basket basket)
+    {
+        if (!basket.Products.Any())
+            return 0;
+
+        var basketValue = basket.Products.Sum(i => i.Price);
+
+        if (basket.User.IsLoggedIn)
+        {
+            return basketValue - (basketValue * 0.05m);
+        }
+
+        return basketValue;
+    }
+}
+
+public class Basket
+{
+    public User User { get; set; }
+    public List<Product> Products { get; set; }
+}
+
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
--- BehaviorDrivenDevelopment.Tests.Behavior/BehaviorDrivenDevelopment.Tests.Behavior/Steps/LoggedInDiscountSteps.cs
+++ BehaviorDrivenDevelopment.Tests.Behavior/BehaviorDrivenDevelopment.Tests.Behavior/Steps/LoggedInDiscountSteps.cs
@@ -5,5 +5,39 @@ namespace BehaviorDrivenDevelopment.Tests.Behavior.Steps;
 [Binding]
 public class LoggedInDiscountSteps
 {
-    
+    [Given(@"a user that is not logged in")]
+    public void GivenAUserThatIsNotLoggedIn()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"an empty basket")]
+    public void GivenAnEmptyBasket()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"a (.*) that costs (.*) GBP is added to the basket")]
+    public void WhenAtShirtThatCostsGbpIsAddedToTheBasket(string itemName, decimal price)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"the basket value is (.*) GBP")]
+    public void ThenTheBasketValueIsGbp(decimal basketValue)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"a user that is logged in")]
+    public void GivenAUserThatIsLoggedIn()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"a dress that costs (.*) GBP is added to the basket")]
+    public void WhenADressThatCostsGbpIsAddedToTheBasket(decimal price)
+    {
+        ScenarioContext.StepIsPending();
+    }
 }
